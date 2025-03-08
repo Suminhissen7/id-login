d9e0d6c670d5b65a2a904477291f5ee056cd1ed6
hassanuzjaman25 <hassanuzjaman25@gmail.com>
2025-03-09 03:40:43 +0600

    Update settings.php

--- info/settings.php
+++ info/settings.php
@@ -3,7 +3,7 @@
 $apiKey = '8fdc3a581fd12d0d6cb8074c8eff6050';
 
 // DATABASE
-$host = 'localhost';
-$user = 'rngamingshop_login_id_my';
-$pass = 'Tbsd$%4sdays';
-$db_name = 'rngamingshop_login_id_my';
+$host = 'mysql-tobd.alwaysdata.net';
+$user = 'tobd_api';
+$pass = 'shihab067%%';
+$db_name = ''tobd_api';
